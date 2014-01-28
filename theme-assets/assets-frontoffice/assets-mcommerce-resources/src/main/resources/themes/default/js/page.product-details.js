7232f3e66ef6ba4eebfd994531f3c47fe950b986
perselab <tringuyen@perselab.com>
2014-01-28 20:42:30 +0700

    Update product-review star

--- theme-assets/assets-frontoffice/assets-mcommerce-resources/src/main/resources/themes/default/js/page.product-details.js
+++ theme-assets/assets-frontoffice/assets-mcommerce-resources/src/main/resources/themes/default/js/page.product-details.js
@@ -18,11 +18,13 @@
 		submitButtonZone: "#submit-button-zone"
 	},
 	_settings = {},
-	_bindEvents = function(){
+	_bindEvents = function(customer){
 		$(_settings.controls.tab).click(_chooseTab);
 		$(_settings.controls.form).submit(_onFormSubmit);
-		$(_settings.controls.reviewButton).click(_review);
-		$(_settings.controls.submitButton).click(_submitForm);
+		if(customer){
+			$(_settings.controls.reviewButton).click(_review);
+			$(_settings.controls.submitButton).click(_submitForm);
+		}
 	},
 	_chooseTab = function(){
 		$(_settings.controls.tab).parent().removeClass("active");
@@ -42,16 +44,19 @@
 			var $star = $("#product-quality-rating");
 			var currentValue = $star.data("star");
 			$star.removeClass("star-"+currentValue).addClass("star-"+value);
+			$star.data("star", value);
 		}else if(type == "price"){
 			_settings.params.ratioQualityPrice = value;
 			var $star = $("#product-price-rating");
 			var currentValue = $star.data("star");
 			$star.removeClass("star-"+currentValue).addClass("star-"+value);
+			$star.data("star", value);
 		}else if(type = "value"){
 			_settings.params.priceScore = value;
 			var $star = $("#product-value-rating");
 			var currentValue = $star.data("star");
 			$star.removeClass("star-"+currentValue).addClass("star-"+value);
+			$star.data("star", value);
 		}						
 		
 		//_submitForm();
@@ -64,8 +69,6 @@
 		$form.find("input#productCommentForm_qualityOfService").val(_settings.params.qualityOfService);
 		$form.find("input#productCommentForm_ratioQualityPrice").val(_settings.params.ratioQualityPrice);
 		$form.find("input#productCommentForm_priceScore").val(_settings.params.priceScore);
-//		$form.find("input[name=page]").val(_settings.params.page);
-//		$form.find("input[name=categoriesFilter]").val(_getCatalogCategories());
 		
 		return true;
 	},
@@ -75,8 +78,15 @@
 	
 	$.product = function(){};
 	
-	$.product.details = function(options){
+	$.product.details = function(){};
+	
+	$.product.details.customer = function(options){
 		_settings = $.extend({}, _DEFAULTS, options);
-		_bindEvents();
+		_bindEvents(true);
+	};
+	
+	$.product.details.guest = function(options){
+		_settings = $.extend({}, _DEFAULTS, options);
+		_bindEvents(false);
 	};
 })(jQuery);
