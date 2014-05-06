a6063a8c6748dd6a88164bc3c4c2e511235bb573
denis <denis@Hoteia-Laptop.home>
2014-05-06 17:18:59 +0200

    update license beforre release

--- webapp-backoffices/webapp-bo-business/src/main/java/org/dozer/MappingProcessor.java
+++ webapp-backoffices/webapp-bo-business/src/main/java/org/dozer/MappingProcessor.java
@@ -1,17 +1,11 @@
-/*
- * Copyright 2005-2010 the original author or authors.
+/**
+ * Most of the code in the Qalingo project is copyrighted Hoteia and licensed
+ * under the Apache License Version 2.0 (release version 0.8.0)
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *                   Copyright (c) Hoteia, 2012-2014
+ * http://www.hoteia.com - http://twitter.com/hoteia - contact@hoteia.com
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 package org.dozer;
 
